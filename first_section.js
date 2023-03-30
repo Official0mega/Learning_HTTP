fd5eca87eadfcc86eb7e47131c85b55fb8e2aadb
Official0mega <supr3m30m3ga@gmail.com>
2023-03-30 18:36:54 +0100

    calculations in js(basi)

---
+++ first_section.js
@@ -0,0 +1,8 @@
+#Web Addresses
+
+Everytime we want to send a HTTP Request
+
+there are Two Steps;
+
+1. Resolve a Domain Name(Domain Name System(DNS)) to an IP Address.
+2. Use the Obtained IP Address to actually make use of it on the Internet.
---
+++ second_section.js
@@ -0,0 +1,8 @@
+// Function to add two variables.
+
+const add = (a, b) => {
+	return a * b
+}
+
+console.log(add(10, 600))
+
